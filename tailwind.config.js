ffeba89490078b7ad0fe271d279e9ae595b22c45
Marc Minkowski <marcminkowski@Mac.localdomain>
2025-11-19 17:13:58 +0100

    fix: optimize tailwind config to exclude node_modules

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,11 @@ export default {
         "./index.html",
         "./src/**/*.{js,ts,jsx,tsx}",
         "./components/**/*.{js,ts,jsx,tsx}",
-        "./app/**/*.{js,ts,jsx,tsx}",
-        "./**/*.{js,ts,jsx,tsx}",
+        "./context/**/*.{js,ts,jsx,tsx}",
+        "./hooks/**/*.{js,ts,jsx,tsx}",
+        "./pages/**/*.{js,ts,jsx,tsx}",
+        "./App.tsx",
+        "./index.tsx"
     ],
     theme: {
         extend: {},
