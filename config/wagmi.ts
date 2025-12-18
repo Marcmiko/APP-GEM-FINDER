4d873af5d240470d24b4ab0328257597330bdfa5
Marc Minkowski <marcminkowski@MacBook-Pro-de-Marc.local>
2025-12-18 12:06:32 +0200

    Standardize configuration and expand security policies

--- config/wagmi.ts
+++ config/wagmi.ts
@@ -1,54 +1,13 @@
 
 import '@rainbow-me/rainbowkit/styles.css';
-import {
-    connectorsForWallets
-} from '@rainbow-me/rainbowkit';
-import {
-    rainbowWallet,
-    walletConnectWallet,
-    coinbaseWallet,
-    metaMaskWallet,
-    trustWallet,
-    ledgerWallet,
-    phantomWallet,
-    rabbyWallet,
-} from '@rainbow-me/rainbowkit/wallets';
+import { getDefaultConfig } from '@rainbow-me/rainbowkit';
 import { base } from 'wagmi/chains';
-import { http, createConfig } from 'wagmi';
 
 const projectId = 'f5281b195c2a7472e1b59d1afc9223d7';
 
-const connectors = connectorsForWallets(
-    [
-        {
-            groupName: 'Recommended',
-            wallets: [
-                coinbaseWallet,
-                walletConnectWallet,
-                metaMaskWallet,
-                rainbowWallet,
-                trustWallet,
-            ],
-        },
-        {
-            groupName: 'Other',
-            wallets: [
-                ledgerWallet,
-                phantomWallet,
-                rabbyWallet,
-            ],
-        },
-    ],
-    {
-        appName: 'MARCMIKO GEM FINDER',
-        projectId,
-    }
-);
-
-export const config = createConfig({
-    connectors,
+export const config = getDefaultConfig({
+    appName: 'MARCMIKO GEM FINDER',
+    projectId,
     chains: [base],
-    transports: {
-        [base.id]: http(),
-    },
+    ssr: false,
 });
